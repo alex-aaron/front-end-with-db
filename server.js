b8bc82e49451f3f45c363895cb12f7386cafaf89
Alex Aaron <alexfrancisaaron@gmail.com>
2025-04-17 19:28:40 +0000

    attempted mongoose connection

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
-  console.log('Hello World');
   res.send("<h1>Hello</h1>")
 });
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
+
+mongoose.connect(
+  'mongodb+srv://mrwigand:SaveMedicare1894@movies.2mzlyap.mongodb.net/Movie-Api?retryWrites=true&w=majority&appName=movies')
+  .then(() => console.log('Connected to database'))
+  .catch((err) => {
+    console.log('Connection failed');
+  })
